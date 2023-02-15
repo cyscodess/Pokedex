c90ab740c7bffeeddd0f653bf5b202f468904b65
Cy <aryssesanvicente@yahoo.com>
2023-02-15 23:55:14 +0800

    Add toggleBgm and toggleBgmIcon functions

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+const bgm = document.getElementById("bgm")
+
 window.onload = () => {
-    const bgm = document.getElementById("bgm")
     bgm.play()
     bgm.volume = 0.05
+    bgm.muted = false
 }
+
+document.getElementById("sound-btn").addEventListener("click", toggleBgm)
+
+function toggleBgm() {
+    bgm.muted = !bgm.muted
+    toggleBgmIcon()
+}
+
+function toggleBgmIcon() {
+    const bgmIcon = document.querySelector("i")
+    if (bgm.muted) {
+        bgmIcon.classList.remove("fa-volume-high")
+        bgmIcon.classList.add("fa-volume-xmark")
+    } else {
+        bgmIcon.classList.add("fa-volume-high")
+        bgmIcon.classList.remove("fa-volume-xmark")
+    }
+}
+
