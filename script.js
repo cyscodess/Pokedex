57fbddd948ee329df824badf80d790050aba7a63
Cy <aryssesanvicente@yahoo.com>
2023-02-18 00:13:44 +0800

    Add display stats functions

--- script.js
+++ script.js
@@ -33,6 +33,10 @@ async function renderPokemon() {
     displayName(pokemon.name)
     displayType(pokemon.types[0].type.name)
     displayImage(pokemon.sprites.front_default)
+    displayHp(pokemon.stats[0].base_stat)
+    displayDef(pokemon.stats[2].base_stat)
+    displayAtk(pokemon.stats[1].base_stat)
+    displaySpAtk(pokemon.stats[3].base_stat)
     displayHeight(pokemon.height)
     displayWeight(pokemon.weight)
     document.getElementById("text-val").value = ""
@@ -61,14 +65,34 @@ function displayImage(pokemon) {
     imageField.src = pokemon
 }
 
+function displayHp(pokemon) {
+    const hpField = document.getElementById("stat-hp")
+    hpField.textContent = `HP: ${pokemon}`
+}
+
+function displayDef(pokemon) {
+    const defField = document.getElementById("stat-def")
+    defField.textContent = `DEF: ${pokemon}`
+}
+
+function displayAtk(pokemon) {
+    const atkField = document.getElementById("stat-atk")
+    atkField.textContent = `ATK: ${pokemon}`
+}
+
+function displaySpAtk(pokemon) {
+    const spAtkField = document.getElementById("stat-sp-atk")
+    spAtkField.textContent = `SP: ${pokemon}`
+}
+
 function displayHeight(pokemon) {
     const htField = document.getElementById("stat-ht")
-    htField.textContent = pokemon
+    htField.textContent = `HT: ${pokemon}`
 }
 
 function displayWeight(pokemon) {
     const wtField = document.getElementById("stat-wt")
-    wtField.textContent = pokemon
+    wtField.textContent = `WT: ${pokemon}`
 }
 
 function clickSound() {
